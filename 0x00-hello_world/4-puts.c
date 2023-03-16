f78b42fcf39381c44a9a54194f05a7266938fe09
leila111111 <leila_merzak@outlook.fr>
2023-03-16 08:22:49 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,16 @@
  * main - Entry point
  *
  * Print : "Programming is like building a multilingual puzzle
+ *
+ * Return 0 if success
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/** 
+	/**
 	 *
 	 * return is a function to return 0
 	 */
-	return(0);
+	return (0);
 }
 

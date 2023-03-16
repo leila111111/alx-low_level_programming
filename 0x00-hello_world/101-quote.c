b968fc7a671c43c54ee285f30df239b5d1eb6e74
leila111111 <leila_merzak@outlook.fr>
2023-03-16 16:11:33 -0700

    my first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main(void)
+
+{
+    int max=120;
+    char message[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
+    for (int count = 0; count < max; count++)  
+    
+    { 
+        if (message[count] == '\0')
+        
+        { 
+            putchar('\n');
+        
+        break; 
+        
+    }
+    else putchar(message[count]);
+    
+    
+}
+
+    return 1;
+}

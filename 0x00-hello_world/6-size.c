f790a1ecf107edb4758739af87d98e62a2c7111f
leila111111 <leila_merzak@outlook.fr>
2023-03-16 11:15:59 -0700

    my first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 {
         printf("Size of a char:%d byte,\n", sizeof(char));
         printf("Size of an int:%d byte,\n", sizeof(int));
-        printf("Size of a long int :%d byte,\n", sizeof(long int);
+        printf("Size of a long int :%d byte,\n", sizeof(long int));
         printf("Size of a long long int:%d byte,\n", sizeof(long long int));
         printf("Size of a float:%d byte,\n", sizeof(float));
         return (0);

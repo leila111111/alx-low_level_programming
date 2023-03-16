d5f565bb2f05d944121c7ce33095991e43d56ea3
leila111111 <leila_merzak@outlook.fr>
2023-03-16 10:33:24 -0700

    my first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+        printf("Size of a char:x:%d byte,\n",sizeof(char));
+	printf("Size of an int:%d byte,\n",sizeof(int));
+	printf("Size of a long int:%d byte,\n",sizeof(long(int)));
+	printf("Size of a long long int:%d byte,\n",sizeof(long(long(int))));
+        return (0);
+}
+~                                                                                  
+~                                                                                  
+~                                                                                  
+~                                                                                  
+~            

a15ca1726f102a1ae09f493963fea81cc30efd19
leila111111 <leila_merzak@outlook.fr>
2023-03-16 16:23:50 -0700

    my first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,26 +3,18 @@
 
 
 int main(void)
-
 {
-    int max=120;
-    int count =0;
-    char message[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
-    for (count = 0; count < max; count++)  
-    
-    { 
-        if (message[count] == '\0')
-        
-        { 
-            putchar('\n');
-        
-        break; 
-        
-    }
-    else putchar(message[count]);
-    
-    
-}
-
-    return 1;
+	int max=120;
+	int count =0;
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	for (count = 0; count < max; count++)
+	{
+		if (message[count] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else putchar(message[count]);
+	}
+	return 1;
 }

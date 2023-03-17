e71589f182a1f6ec87c9db0d03d56b331167d00a
leila111111 <leila_merzak@outlook.fr>
2023-03-17 14:41:49 -0700

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i='0';
+	for (j = 0; j < 10; j++)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

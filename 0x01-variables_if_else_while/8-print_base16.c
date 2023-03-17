874c89efe955f5d3ad56793e1cc855a3f087fbe9
leila111111 <leila_merzak@outlook.fr>
2023-03-17 14:55:49 -0700

    my commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alphabet[] = "0123456789abcdef";
+
+	for (i = 0; i < 15; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}

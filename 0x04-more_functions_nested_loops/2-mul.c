ab4f43fae1b5499c74c4294bd2bee8865df21973
leila111111 <leila_merzak@outlook.fr>
2023-03-23 15:31:04 -0700

    my commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,11 +10,11 @@ int mul(int a, int b)
 {
 	int mul;
 	
-	if ((a > 0 && b > 0) || (a < 0 && b < 0)
+	if ((a > 0 && b > 0) || (a < 0 && b < 0))
 	{
 		mul = a*b;
 	}
-	if ((a > 0 && b < 0) || (a < 0 && b > 0)
+	if ((a > 0 && b < 0) || (a < 0 && b > 0))
 	{
 		mul = -a*b;
 	}

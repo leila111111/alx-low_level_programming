787d1556d74afc99aac10f1994c48aeec28d3a26
leila111111 <leila_merzak@outlook.fr>
2023-03-22 18:44:36 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry point, Priting _putchar
+ * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+	int i;
+	char array[] = "_putchar";
+
+	for (i = 0; i < 10; i++)
+	{
+		if (array[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		else
+			_putchar(array[i]);
+	}
+return (0);
 }

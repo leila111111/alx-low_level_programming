a368ee784a2e40a2627103bb2b443ac321c4bf9c
leila111111 <leila_merzak@outlook.fr>
2023-03-22 19:11:33 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function  adding two integers and returning the sum
+ * @a: first  variables to enter
+ * @b: second number to enter
+ * Return: somme
+ */
+int add(int a, int b)
+{
+	int somme = a + b;
+
+return (somme);
+}

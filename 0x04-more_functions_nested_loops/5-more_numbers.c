78f6d3745112286f31da49fa130e10e86534214f
leila111111 <leila_merzak@outlook.fr>
2023-03-23 17:29:02 -0700

    my commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- * print_numbers - function that prints the numbers, from 0 to 9
- * Return: void
+ * more_numbers -  function that prints 10 times the numbers
+ * Return: nothing
  */
 void more_numbers(void)
 {
-        char marks[20]={'0','1','2','3','4','5','6','7','8','9','1','0','1','1','1','2','1','3','1','4'};
-        int j;
-        int k;
-        for (k =0 ;k <= 9 ;k++)
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
 	{
-                for (j = 0; j <= 20; j++)
+		for (j = 0; j <= 14; j++)
 		{
-		       	_putchar(marks[j]);
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-                 _putchar('\n');
+		_putchar('\n');
 	}
 }

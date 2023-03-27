8700a4d8751f2cbbf6215f9417f392d704f64844
leila111111 <leila_merzak@outlook.fr>
2023-03-27 14:54:22 +0000

    my commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+#include <string.h>
+/**
+ * _strlen - check the code
+ *
+ * @s: The parameter to enter
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
+

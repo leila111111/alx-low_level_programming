677bbeb8f8231c6807ffd32c55cc7925e792cf4a
leila111111 <leila_merzak@outlook.fr>
2023-03-27 14:36:56 +0000

    my commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ *
+ * @a: number 1 to enter
+ * @b: number 2 to enter
+ * Return: Always nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int p = *a; 
+	*a = *b;
+	*b = p;
+}

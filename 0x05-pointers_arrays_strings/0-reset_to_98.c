96e67c0ef864f7fdb5ce2bba80afca0fbe0e4b68
leila111111 <leila_merzak@outlook.fr>
2023-03-27 14:00:54 +0000

    my commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98 - check the code 
+ * reset_to_98 - check the code
  *
+ * @n: is the number to enter
  * Return: Always 0
  */
 void reset_to_98(int *n)

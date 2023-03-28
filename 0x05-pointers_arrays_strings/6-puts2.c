2d0b481ee5a6cc3e8da5212d3bdcad7a45d50198
leila111111 <leila_merzak@outlook.fr>
2023-03-28 00:29:59 +0000

    my commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - check the function
+ * @str: string to enter
+ */
+void puts2(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (str[i] != '0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j +=2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}	

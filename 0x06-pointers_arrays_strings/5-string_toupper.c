e91381b57ebba6c86d974e0a61617c19248df481
leila111111 <leila_merzak@outlook.fr>
2023-03-30 15:57:25 -0700

    my commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - function that changes all lowercase letters of a string to uppercase
+ * string_toupper - function that changes ..
  * @s : string
  * Return: the resulting string
  */

e572b837fe6caffc3e1c5ae3210fe215fdb6f0c6
leila111111 <leila_merzak@outlook.fr>
2023-04-06 16:33:59 -0700

    my commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,14 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	_putchar(*(s + i));
+	i++;
+	if (*(s + i) != '\0')
 	{
-		_putchar(*(s + i));
-		i++;
+		_puts_recursion(s + i);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }

f37bdcaa099f99235e5d0507c091973fe944989e
leila111111 <leila_merzak@outlook.fr>
2023-04-06 17:07:43 -0700

    my commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: number to enter
+ * Return: -1 if n is negative sinon  return  factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+                return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}

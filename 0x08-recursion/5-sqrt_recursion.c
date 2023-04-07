0126b9c547ed7343985a2045d24b6966abb7c67b
leila111111 <leila_merzak@outlook.fr>
2023-04-06 17:27:43 -0700

    my commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: number
+ * Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	return (_sqrt_recursion_iteration(n, i));
+}
+
+/**
+ * _sqrt_recursion_iteration - function help to find square root
+ * @x: square root
+ * @n:  number
+ * Return: int
+ */
+
+int _sqrt_recursion_iteration(int n, int i)
+{
+	if (n < i * i)
+		return (-1);
+	if (n == i * i)
+		return (i);
+	if (n == 0)
+		return (0);
+	return (_sqrt_recursion_iteration(n, i + 1));
+}

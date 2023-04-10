1011bbfb28cafbb2416139224966b14eb3c610ca
leila111111 <leila_merzak@outlook.fr>
2023-04-10 15:29:03 -0700

    my commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: number of arguments
+ *@argv: arguments
+ * Return: Always 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum;
+
+	if (argc > 1)
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] > '9' || argv[i][j] < '0')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum = sum + atoi(argv [i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+	if (argc == 1)
+		printf("0\n");
+}

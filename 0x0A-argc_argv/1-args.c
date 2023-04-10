47120c8db31aa62616d2396bc22f13306877eafc
leila111111 <leila_merzak@outlook.fr>
2023-04-10 10:34:25 -0700

    my commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  *@argv: arguments
  * Return: Always 1 (Success)
  */
-int main(int argc , char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-        printf("%d\n", argc-1);
-        return (0);
-}      
+	printf("%d\n", argc - 1);
+	return (0);
+}

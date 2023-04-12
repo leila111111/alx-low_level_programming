2a09ff66f02ec4d736c8c826de74ddcc042ea662
leila111111 <leila_merzak@outlook.fr>
2023-04-11 17:49:25 -0700

    my commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - function
+ * @width: width
+ * @height: height
+ * Return: Null or pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = (int **) malloc(sizeof(int *) * height);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = (int *) malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+		{
+			free(p);
+			for (j = 0; j <= i; j++)
+				free(p[j]);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	
+	return (p);
+}

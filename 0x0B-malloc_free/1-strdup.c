6cccea01dc07a97ab4fa7048b7b526e8888af90f
leila111111 <leila_merzak@outlook.fr>
2023-04-11 17:02:06 -0700

    my commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ * _strdup - a function that returns a pointer to a newly allocated space
+ * @str: input
+ * Return: Null or p
+ */
+char *_strdup(char *str)
+{
+	int i, j = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[j])
+		j++;
+	p = (char *)malloc(j + 1);
+	if (p == NULL)
+		return (NULL);
+	while ((p[i] = str[i]) != '\0')
+	{
+		i++;
+	}
+	return (p);
+}

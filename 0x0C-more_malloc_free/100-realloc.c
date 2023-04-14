58e3d39b53e9712d7dcc8d88c5ba2cbf77973777
leila111111 <leila_merzak@outlook.fr>
2023-04-14 16:56:09 -0700

    my commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,2 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_realloc - function tht reallocates a memory block using malloc and free
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer or NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr1;
+	unsigned int i;
 
-io
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		ptr1 = (malloc(new_size));
+		return (ptr1);
+
+	ptr1 = (malloc(new_size));
+	if (ptr1 == NULL)
+		return (NULL);
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = ptr[i];
+	}
+	free(ptr);
+	return (ptr1);
+}

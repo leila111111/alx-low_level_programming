9e136e0bb36e5171df4c8cb4412d0bead248b8a5
leila111111 <leila_merzak@outlook.fr>
2023-04-14 16:31:12 -0700

    my commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,24 @@
-i
+#include <stdlib.h>
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: the number of elements in the array
+ * @size: size of the element
+ * Return: pointer or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *pointer;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	pointer = malloc(nmemb * size);
+
+	if (pointer == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		pointer[i] = 0;
+	return (pointer);
+}

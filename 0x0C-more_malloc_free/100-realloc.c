a514e3ae99ee3f6febbbca239ebde6fe15c253d2
leila111111 <leila_merzak@outlook.fr>
2023-04-14 17:08:34 -0700

    my commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,13 +28,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ptr1[i] = ((char*)ptr)[i];
+			ptr1[i] = ((char *)ptr)[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr1[i] = ((char*)ptr)[i];
+			ptr1[i] = ((char *)ptr)[i];
 	}
 	free(ptr);
 	return (ptr1);

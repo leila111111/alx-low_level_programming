c824b7742cccaec57f1fd3c41b1f5dd77be36260
leila111111 <leila_merzak@outlook.fr>
2023-04-20 17:15:31 -0700

    my commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+
+	va_list var;
+
+	va_start(var, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(var, char *);
+		if (!s)
+			s = "(nil)";
+		if (!separator)
+			printf("%s", s);
+		else if (separator && i == 0)
+			printf("%s", s);
+		else
+			printf("%s%s", separator, s);
+	}
+	printf("\n");
+	va_end(var);
+}

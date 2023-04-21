1f33f4b713bc6a9734720f960be21addbfa46e1a
leila111111 <leila_merzak@outlook.fr>
2023-04-20 17:35:05 -0700

    my commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,16 +1,17 @@
 #include "variadic_functions.h"
+
 /**
- * print_all - print
- * @format: arguments
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char *s, *separator = "";
+	char *str, *sep = "";
 
-	va_list var;
+	va_list list;
 
-	va_start(var, format);
+	va_start(list, format);
 
 	if (format)
 	{
@@ -19,28 +20,29 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", s, va_arg(var, int));
+					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
-					printf("%s%d", s, va_arg(var, int));
+					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%s%f", s, va_arg(var, double));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
-					s = va_arg(var, char *);
-					if (!s)
-						s = "(nil)";
-					printf("%s%s", separator, s);
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
 					break;
 				default:
 					i++;
 					continue;
 			}
-			separator = ", ";
+			sep = ", ";
 			i++;
 		}
 	}
+
 	printf("\n");
-	va_end(var);
+	va_end(list);
 }

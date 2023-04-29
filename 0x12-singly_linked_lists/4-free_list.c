55a63eb65cf0e4f61a53dbe05fa1b45e470ead20
leila111111 <leila_merzak@outlook.fr>
2023-04-29 16:12:02 -0700

    my commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head node of list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}

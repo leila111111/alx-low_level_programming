c89060ac331862c341091b3231ff4e5fa99d924a
leila111111 <leila_merzak@outlook.fr>
2023-04-29 13:22:56 -0700

    my commit

--- 0x0F-function_pointers/0-print_list.c
+++ 0x0F-function_pointers/0-print_list.c
@@ -11,10 +11,10 @@ size_t print_list(const list_t *h)
 	const listint_t *current = h;
 
 	while (current != NULL)
-	{	
+	{
 		printf("%d\n", current->n);
 		current = current->next;
 		count++
 	}
-	return count;
+	return (count);
 }

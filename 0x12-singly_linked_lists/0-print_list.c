a78171536c502e4b15b9d76246687f7ff4317ae9
leila111111 <leila_merzak@outlook.fr>
2023-04-29 13:54:05 -0700

    my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,13 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 
 	while (h)
-		{
-			if (!h->str)
-				printf("[0] (nil)\n");
-			else
-				printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			count++;
-		}
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
 	return (count);
 }

48a5d716936a42b0f5d58667fb91df15de92942e
leila111111 <leila_merzak@outlook.fr>
2023-04-29 15:39:04 -0700

    my commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,8 +9,9 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, 
-	list-t *last_node = *head;
+	list_t *new_node;
+
+	list_t *last_node = *head;
 	unsigned int i = 0;
 
 	while (str[i])
@@ -18,8 +19,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (!new_node == NULL)
 		return (NULL);
-	new_node ->i = i;
-	new_node ->next = NULL;
+	new_node->i = i;
+	new_node->next = NULL;
 	new_node->str = strdup(str);
 
 	if (*head == NULL)

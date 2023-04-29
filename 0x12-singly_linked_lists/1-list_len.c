81a541831553b19cd542c0a9edb764d129eba183
leila111111 <leila_merzak@outlook.fr>
2023-04-29 14:22:10 -0700

    my commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * listint_len - Returns the number of elements in a linked listint_t list.
+ * @h: A pointer to the head node of the list.
+ * Return: The number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

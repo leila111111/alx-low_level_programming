23e937b93cdb8c7a4b5fde8ad1e0fec48fcf72de
leila111111 <leila_merzak@outlook.fr>
2023-04-29 12:46:32 -0700

    my commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (atoi(argv[3]) == 0 && (argv[2][0] == '%' || argv[2][0] == '/'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	i = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", i);
+	return (0);
+}
+		
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - function that calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that calculates the product of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function that calculates the %  of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: %
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

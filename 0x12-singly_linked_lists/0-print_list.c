5aea72b4576003a61ee6a75186ce82912f21d7c6
leila111111 <leila_merzak@outlook.fr>
2023-04-29 13:35:50 -0700

    my commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ * Return: The number of nodes in the listint_t list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+		{
+			printf("%d\n", current->n);
+			current = current->next;
+			count++;
+		}
+	return count;
+}

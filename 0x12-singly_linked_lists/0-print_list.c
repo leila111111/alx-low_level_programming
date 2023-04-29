75631a4227c84ea2cb37b46bbb9607758e51289e
leila111111 <leila_merzak@outlook.fr>
2023-04-29 14:09:19 -0700

    my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ * print_list - Prints all the elements of a listint_t list.
  * @h: A pointer to the head of the listint_t list.
  * Return: The number of nodes in the listint_t list.
  */

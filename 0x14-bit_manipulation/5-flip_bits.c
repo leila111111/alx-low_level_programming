eb4b69b614788aa520f7ee2d1d4a5a36174f2ce6
leila111111 <leila_merzak@outlook.fr>
2023-05-21 12:27:50 -0700

    my commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * flip_bits - returns the number of bits needed to flip to get from n to m
+ * @n: numberone
+ * @m: numbertwo
+ * Return:the bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int expo;
+	unsigned int count = 0;
+	
+	expo = n ^ m;
+	while (expo > 0)
+	{
+		count += expo & 1;
+		expo >>= 1;
+	}
+
+	return count;
+}

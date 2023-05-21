200c2a1d1a755baafd0192fa7adda1eb171cf876
leila111111 <leila_merzak@outlook.fr>
2023-05-21 10:11:36 -0700

    my commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,12 +7,12 @@
  */
 void print_error(char *file, int i)
 {
-	if(i == 98)
+	if (i == 98)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 		exit(98);
 	}
-	if(i == 99)
+	if (i == 99)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
@@ -36,17 +36,17 @@ int main(int argc, char *argv[])
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error(argv[1],98);
+		print_error(argv[1], 98);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		print_error(argv[2],99);
+		print_error(argv[2], 99);
 	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		if (bytes_read == -1)
-			print_error(argv[1],98);
+			print_error(argv[1], 98);
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
-			print_error(argv[2],99);
+			print_error(argv[2], 99);
 	}
 	if (close(fd_from) == -1)
 	{
@@ -58,5 +58,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+	close(fd_from);
+	close(fd_to);
 	return (0);
 }

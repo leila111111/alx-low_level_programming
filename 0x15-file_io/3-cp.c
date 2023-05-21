3c9e2581b5a2f256cca214c7bbcd94d396502017
leila111111 <leila_merzak@outlook.fr>
2023-05-21 09:50:43 -0700

    my commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * print_error - printing error
+ * @file: the file
+ * @i: the value of error
+ * Return: nothing
+ */
+void print_error(char *file, int i)
+{
+	if(i==98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		exit(98);
+	}
+	if(i==99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+}
+/**
  * main - Copies the content of one file to another file.
  * @argc: The number of arguments.
  * @argv: An array of argument.
@@ -17,37 +36,23 @@ int main(int argc, char *argv[])
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_error(arg[1],98);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-                exit(99);
-	}
-
+		print_error(arg[2],99);
 	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		if (bytes_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			print_error(arg[1],98);
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-                	exit(99);
-		}
+			print_error(arg[2],99);
 	}
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);

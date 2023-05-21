ced53e158f98cadcea89ff40d839a300b544a595
leila111111 <leila_merzak@outlook.fr>
2023-05-21 10:56:36 -0700

    my commit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,11 @@
+#include "main.h"
 /**
- * get_endianness - Checks the endianness of the system
- *
+ * get_endianness - Checks the endiannes
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	unsigned int num = 1;
 	char *byte = (char *)&num;
-
-	/* If the value at the first byte is 1, it means the system is little endian */
-	/* Otherwise, it is big endian */
 	return (int)byte[0];
 }

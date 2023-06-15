fc6ef66ea28f69788812087281453b4d91cd6df0
leila111111 <leila_merzak@outlook.fr>
2023-06-15 16:58:05 -0700

    my commit

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum of the data values in the list.
+ * @head: A pointer to the head of the list.
+ * Return: sum or 0.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *list = head;
+	int sum = 0;
+
+	while (list != NULL)
+	{
+		sum += list->n;
+		listt = list->next;
+	}
+	return (sum);
+}

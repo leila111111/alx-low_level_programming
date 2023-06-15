38281705eeb67bff9fff4bb0f6bfb66fd976add5
leila111111 <leila_merzak@outlook.fr>
2023-06-15 16:44:35 -0700

    my commit

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free the list.
+ * @header: A pointer to the head of the list.
+ * Return: void function
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *list;
+
+	while (head != NULL)
+	{
+		list = head;
+		head = head->next;
+		free(list);
+	}
+}

e4d71ae4d62e6dc4b203edc2cedcadb70b5389ad
leila111111 <leila_merzak@outlook.fr>
2023-08-23 22:14:40 +0000

    permieter island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
+
     int perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
